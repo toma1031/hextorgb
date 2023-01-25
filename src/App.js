c5ef137fe3b5f58f4f1a522d2435d3378ac75770
Akinori Toma <akinoritoma@appletoma.local>
2023-01-25 02:18:25 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+
+  const [hex_value, setHexValue] = useState("#fffff");
+  
+  const hexToRGB = () => {
+     const value = parseInt( hex_value.slice(1),16);
+     let r = value >> 16 & 0xFF;
+     let g = value >> 8 & 0xFF;
+     let b = value & 0xFF;
+     return `[${r}, ${g}, ${b}]`;
+  }
+
+  const handleChange = (e) => {
+    setHexValue(e.target.value)
+    console.log(e.target.value)
+  }
+
+  // console.log(hex_value)
+  // console.log(hexToRGB())
+
+    
+  useEffect(() => {
+      }, []);
+    
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 className="title">HEX To RGB</h1>
+      <div className="color_pikcer_section">
+        <input type="color" className="color_pikcer_bar" onChange={handleChange}/>
+        <p>{hexToRGB()}</p>
+      </div>
+    </>
   );
 }
 
